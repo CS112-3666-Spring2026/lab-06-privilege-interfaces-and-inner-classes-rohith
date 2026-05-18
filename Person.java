bacc4d4c3797c7c52c62ca6b677cdb19bfa31b80
rmekala3623 <rmekala3623@student.miracosta.edu>
2026-05-18 03:33:46 +0000

    Finished commit.

--- Person.java
+++ Person.java
@@ -1,31 +1,59 @@
 public class Person implements Comparable
 {
 	/***** TODO: (Part 2) create helper inner class for Identity*****/
-	
+	private class Identity	{
+		private String pronouns, background;
+
+		public Identity(String pronouns, String background) {
+			this.pronouns = pronouns;
+			this.background = background;
+		}
+
+		public Identity() {
+			this(Person.DEFAULT_PRONOUNS, Person.DEFAULT_BACKGROUND);
+		}
+
+		@Override
+		public String toString() {
+			return "my personal pronouns are " + this.pronouns + " and " + this.background;
+		}
+
+		@Override
+		public boolean equals(Object other) {
+			if (other == null || this.getClass() != other.getClass()) {
+				return false;
+			}
+
+			Identity otherIdentity = (Identity) other;
+			return this.pronouns.equals(otherIdentity.pronouns) && this.background.equals(otherIdentity.background);
+		}
+	} //End of Identity inner class
 
 	// CONSTANT VARIABLES
 	public static final String DEFAULT_NAME = "Jamie Doe";
-	public static final String DEFAULT_STORY =  "Unknown";
+	public static final String DEFAULT_PRONOUNS = "they/them/their";
+	public static final String DEFAULT_BACKGROUND =  "I am an Asian American computer science major";
 	public static final int DEFAULT_PRIVILEGE = 100;
 
 	// INSTANCE VARIABLES
-	private String name, story;
+	private String name;
+	private Identity story = new Identity();
 	private int privilege;
 
 	// CONSTRUCTORS	
-	public Person(String name, String story, int privilege) {
-		this.setAll(name, story, privilege);
+	public Person(String name, String pronouns, String background, int privilege) {
+		this.setAll(name, pronouns, background, privilege);
 	}
 		
 	public Person() {
-		this(DEFAULT_NAME, DEFAULT_STORY, DEFAULT_PRIVILEGE);
+		this(Person.DEFAULT_NAME, Person.DEFAULT_PRONOUNS, Person.DEFAULT_BACKGROUND, Person.DEFAULT_PRIVILEGE);
 	}
 	
 	public Person(Person original) {
 		if(original == null) {
 			throw new IllegalArgumentException("Cannot copy null obect in Person copy constructor");
 		} else {
-			this.setAll(original.name, original.story, original.privilege);
+			this.setAll(original.name, original.story.pronouns, original.story.background, original.privilege);
 		}
 	}
 
@@ -34,27 +62,36 @@ public class Person implements Comparable
 		this.name = name;
 	}
 
-	public void setStory(String story) {
-		this.story = story;
+	public void setPronouns(String pronouns) {
+		this.story.pronouns = pronouns;
+	}
+
+	public void setBackground(String background) {
+		this.story.background = background;
 	}
 
 	public void setPrivilege(int privilege) {
 		this.privilege = privilege;
 	}
 
-	public void setAll(String name, String story, int privilege) {
+	public void setAll(String name, String pronouns, String background, int privilege) {
 		this.setPrivilege(privilege);
 		this.setName(name);
-		this.setStory(story);
+		this.setPronouns(pronouns);
+		this.setBackground(background);
 	}
 
 	// ACCESSORS / GETTERS
 	public String getName() {
 		return this.name;
 	}
-		
-	public String getStory() {
-		return this.story;
+	
+	public String getPronouns() {
+		return this.story.pronouns;
+	}
+
+	public String getBackground() {
+		return this.story.background;
 	}
 
 	public int getPrivilege() {
@@ -65,7 +102,7 @@ public class Person implements Comparable
 	@Override
 	public String toString()
 	{
-		return "My name is "+ this.name + " and "+ this.story + "\n"
+		return "My name is " + this.name + ", " + this.story + "\n"
 				+ "According to this calculator I ended up with "+ this.privilege + " estimated privilege points";
 	}
 	
